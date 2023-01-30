c1c246ba15358d106ce4f3e17b461f2f9bd7f2bd
Max <dannyjimenez12345@gmail.com>
2023-01-30 17:15:12 -0600

    Create function operate that calls basic math functions

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 };
+
+function operate (operator, num1, num2) {
+    if (operator === '+') return add(num1,num2);
+    if (operator === '-') return subtract(num1,num2);
+    if (operator === '*') return multiply(num1,num2);
+    if (operator === '/') return num2 === 0 ? "Can't divide by 0" : divide(num1,num2);
+}
+// console.log (operate('/',20,4))
